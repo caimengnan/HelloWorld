601f524ab787350829147e151b2c065dbf5c58ec
caimengnan <570788791@qq.com>
2021-06-10 13:57:37 +0800

    first commit

---
+++ HelloWorld/Classes/LZPerson.h
@@ -0,0 +1,17 @@
+//
+//  LZPerson.h
+//  OC Test
+//
+//  Created by The Man In Your Dream on 2021/6/10.
+//  Copyright © 2021 The Man In Your Dream. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LZPerson : NSObject
+- (void)testPrint:(NSString *)text;
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ HelloWorld/Classes/LZPerson.m
@@ -0,0 +1,20 @@
+//
+//  LZPerson.m
+//  OC Test
+//
+//  Created by The Man In Your Dream on 2021/6/10.
+//  Copyright © 2021 The Man In Your Dream. All rights reserved.
+//
+
+#import "LZPerson.h"
+
+@implementation LZPerson
+
+
+- (void)testPrint:(NSString *)text
+{
+    NSLog(@"LZPerson print == %@",text);
+}
+
+
+@end
